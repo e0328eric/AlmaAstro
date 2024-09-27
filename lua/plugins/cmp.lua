638c83e10c5da351b3c88856fad7d10ea8cff5ac
Sungbae Jeong <almagest0328@gmail.com>
2024-09-27 13:45:46 +0900

    asdsadsad

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,8 @@ return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
     local cmp = require "cmp"
+    local luasnip = require "luasnip"
+
     opts.mapping["<C-]>"] = function(fallback)
       if not cmp.select_next_item() then
         if vim.bo.buftype ~= "prompt" and has_words_before() then
@@ -17,6 +19,7 @@ return {
         end
       end
     end
+
     opts.mapping["<A-]>"] = function(fallback)
       if not cmp.select_prev_item() then
         if vim.bo.buftype ~= "prompt" and has_words_before() then
@@ -26,5 +29,21 @@ return {
         end
       end
     end
+
+    opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+
+    opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" })
   end,
 }
