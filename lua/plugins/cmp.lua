7452a07f121a3e539351943cc6bb9fd426e7fcb6
Sungbae Jeong <almagest0328@gmail.com>
2024-09-27 13:40:36 +0900

    asdsad

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,30 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    opts.mapping["<C-]>"] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= "prompt" and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end
+    opts.mapping["<A-]>"] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= "prompt" and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end
+  end,
+}
